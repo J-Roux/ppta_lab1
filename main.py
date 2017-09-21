1a07ceb3972cbdf830f3d349e61af93a8ada6cbb
J-Roux <aps7@tut.by>
2017-09-21 12:09:36 +0300

    init

---
+++ main.py
@@ -0,0 +1,118 @@
+import unittest
+
+
+def is_terminal(string, Vt):
+    if len(string) == 1 and string in Vt:
+        return True
+    else:
+        return False
+
+def is_nonterminal(string, Vn):
+    if len(string) == 1 and string in Vn:
+        return True
+    else:
+        return False
+
+def is_rule_type3(string, Vt, Vn):
+    if len(string) == 2:
+        if (is_terminal(string[0], Vt) and is_nonterminal(string[0]), Vn) or\
+                (is_nonterminal(string[0], Vn) and is_terminal(string[1]), Vt):
+            return True
+        else:
+            return False
+    if len(string) == 1 and is_terminal(string, Vt):
+        return True
+    return False
+
+
+def is_type3(P, Vt, Vn):
+    if all([ is_nonterminal(i, Vn) for i in P.keys()]) and \
+            all([all([ is_rule_type3(rule, Vt, Vn) for rule in rules]) for rules in P.values()]) :
+        return True
+    else:
+        return False
+
+def is_type2(P, Vn):
+    if all ([is_nonterminal (i, Vn) for i in P.keys ()]):
+        return True
+    else:
+        return False
+
+
+
+
+def is_type1(P):
+    if all([all([ len(i) >= len(key) for i in P[key] ]) for key in P]):
+        return True
+    else:
+        return False
+
+def define_grammar_type(P, Vt, Vn):
+    if not is_type3(P, Vt, Vn):
+        if not is_type2(P,Vn):
+            if not is_type1(P):
+                return 'type0'
+            else:
+                return 'type1'
+        else:
+            return 'type2'
+    else:
+        return 'type3'
+
+
+class Test (unittest.TestCase):
+    # S -> aSBC | abc
+    # bCBCC -> bc
+    # CBa -> BC
+    # cC -> cc
+
+    def test_type0 ( self ):
+        Vt = ['a', 'b', 'c']
+        Vn = ['S', 'C', 'B']
+        P = {'S': ['aSBC', 'abc'], 'bCBCC': ['bc'], 'CBa': ['BC'], 'cC': ['cc']}
+        self.assertEqual(define_grammar_type(P, Vt, Vn), 'type0')
+
+    # S -> aSBC | abc
+    # bC -> bc
+    # CB -> BC
+    # cC -> cc
+
+    def test_type1 ( self ):
+        Vt = ['a', 'b', 'c']
+        Vn = ['S', 'C', 'B']
+        P = {'S': ['aSBC', 'abc'], 'bC': ['bc'], 'CB': ['BC'], 'cC': ['cc']}
+        self.assertEqual(define_grammar_type(P, Vt, Vn), 'type1')
+
+    # S -> aQb | accb
+    # Q -> cSc
+
+    def test_type2 ( self ):
+        Vt = ['a', 'b', 'c']
+        Vn = ['Q', 'S']
+        P = {'S': ['aQb', 'accb'], 'Q': ['cSc']}
+        self.assertEqual(define_grammar_type(P, Vt, Vn), 'type2')
+
+    # S -> A& | B&
+    # A -> a | Ba
+    # B -> b | Bb | Ab
+
+    def test_type3 ( self ):
+        Vt = ['a', 'b', '&']
+        Vn = ['A', 'B', 'S']
+        P = {'S': ['A&', 'B&'], 'A': ['a', 'Ba'], 'B': ['b', 'Bb', 'Ab']}
+        self.assertEqual(define_grammar_type(P, Vt, Vn), 'type3')
+
+    # S -> Aa | Ab
+    # A -> Bc
+    # B -> Ca | Cb
+    # C -> Dc
+    # D -> a | b
+
+    def test_my_grammar(self):
+        Vt = ['a', 'b', 'c']
+        Vn = ['S', 'A', 'B', 'C', 'D']
+        P = { 'S' : ['Aa', 'Ab'], 'A' : ['Bc'], 'B' : ['Ca', 'Cb'], 'C' : ['Dc'], 'D' : ['a', 'b']}
+        self.assertEqual(define_grammar_type(P, Vt, Vn), "type3")
+
+if __name__ == '__main__':
+    unittest.main()
